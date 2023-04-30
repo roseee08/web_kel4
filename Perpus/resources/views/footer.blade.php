afdcb9461adc961204ef98aad5187291493096a7
roseee08 <roseenjellina@gmail.com>
2023-04-30 16:48:14 +0700

    first commit

---
+++ Perpus/resources/views/footer.blade.php
@@ -0,0 +1,27 @@
+<p>
+    <!--awal FOOTER-->
+    <div class="col-md-12">
+        <div class="row" style="background:rgba(63, 67, 102, 0.562)">
+            <div class="col-md-9">
+                <h3>Visi</h3>
+                <p>Sebagai penyedia informasi ilmu pengetahuan dan teknologi
+
+                <p>
+                <h3>Misi</h3>
+                <ul>
+                    <li>Meningkatkan relevansi dan mutu layanan sitem perpustakaan terpadu kepada seluruh pemakai jasa di lingkungan universitas.</li>
+                    <li>Menerapkan teknologi informasi, teknologi yang pada intinya bertumpu pada konsep otomasi.</li>
+                    <li>Perpustakaan tidak hanya sebagai pemakai tetapi juga sebagai pemilik data dan informasi dalam rangka mendukung kerjasama (resources sharing ) dengan perpustakaan dan pusat informasi lain.</li>
+                </ul>                     
+            </div>
+            <div class="col-md-3">
+                Copyright &copy; 2023. All right reserve
+                <p>
+                Developed by Kelompok 4 </div>
+        </div>
+    </div>
+    <!--akhir FOOTER-->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+       
---
+++ Perpus/resources/views/view_home.blade.php
@@ -0,0 +1,25 @@
+@extends('index')
+@section('title', 'Home')
+
+@section('isihalaman')
+    <h3>Pengertian perpustakaan menurut para ahli</h3>
+    <p>
+    <h4>Perpustakaan</h4>
+        Pengertian perpustakaan menurut pandangan Sulistyo Basuki secara umum adalah bagian dari sebuah gedung biasanya
+        digunakan untuk tempat penyimpanan buku dan lainnya yang terdapat pada sebuah ruangan dan di tata menurut susunan
+        tertentu untuk menyediakan dan digunakan pembaca bukan untuk dijual
+    <p>
+
+    <h4>Perpustakaan Perguruan Tinggi</h4>
+        Perpustakaan Perguruan Tinggi merupakan unit kerja pelaksana teknis (UPT) Perguruan Tinggi yang bersama-sama 
+		dengan unit lain turut melaksanakan Tri Dharma Perguruan Tinggi, yaitu: pendidikan, penelitian, dan pengabdian 
+		kepada masyarakat dengan cara memilah, menghimpun, mengolah, merawat, serta menyebarluaskan sumber informasi kepada 
+		lembaga induknya pada khususnya dan sivitas akademika pada umumnya. Kelima tugas tersebut dilaksanakan dengan 
+		tata cara administrasi dan organisasi yang berlaku bagi penyelenggaraan sebuah perpustakaan.
+        
+    <p>
+    <h4>Perpustakaan Menurut UU NO 43. THN. 2007</h4>
+    Perpustakaan adalah institusi yang mengumpulkan pengetahuan tercetak dan terekam, mengelolanya dengan cara khusus guna
+    memenuhi kebutuhan intelektualitas para penggunanya melalui berbagai cara interaksi pengetahuan.
+
+@endsection
